1f741970bd10f088711126ae9cdc012d0877d747
Michal Scevlik <vianocka@gmail.com>
2011-04-05 10:22:35 +0200

    Prvy trapny commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arxiv::Application.config.secret_token = '82b72b0082f3e45002d1a907a5f1f90e17f9fc3b9b6e5198596a2996bcd94a363cd66afb457432d726901c44cc59c893c4d73314192131a509c45f60f6d7efae'
